790ad8396e45601d8e41c375599331e2f307e943
ashishmangla <ashishmangla1991@gmail.com>
2021-06-11 16:48:15 +0530

    add header

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -1,12 +1,25 @@
-const Header = () =>{
-    return(
-        <nav>
-            <div className="" role="list">
-                <Link></Link>
-            </div>
-        </nav>
-    )
-}
+import Link from 'next/link';
+import style from './style.module.scss';
+const Header = () => {
+  return (
+    <nav className={style.header}>
+      <div className="" role="list">
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/">
+          <a>about</a>
+        </Link>
 
+        <Link href="/">
+          <a>projects</a>
+        </Link>
+        <Link href="/">
+          <a>contacts</a>
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
-export default Header
+export default Header;
