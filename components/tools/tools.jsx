2d5e8a5d792557512d72f6c1d3164edf1795c672
ashishmangla <ashishmangla1991@gmail.com>
2021-07-14 23:16:40 +0530

    add new component

---
+++ components/tools/tools.jsx
@@ -0,0 +1,14 @@
+import Section from "../section/section";
+import style from './tool.module.scss';
+const Tools = () => {
+  return (
+  <Section className={style.tools}>
+      <div className={style.tools__pattren1}></div>
+      <div className={style.tools__pattren2}></div>
+      <div className="container">
+          asdf df asfsdf sfsdf sdfs dfs
+      </div>
+  </Section>
+  )
+};
+export default Tools;
