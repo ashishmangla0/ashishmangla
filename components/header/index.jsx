aaa34b48a3fb53fda18fec2d2575932102256ef7
ashishmangla <ashishmangla1991@gmail.com>
2021-07-14 22:10:45 +0530

    add google fonts

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -1,11 +1,11 @@
-import Link from 'next/link';
-import style from './style.module.scss';
+import Link from "next/link";
+import style from "./style.module.scss";
 const Header = () => {
   return (
     <nav className={style.header}>
-      <div className="" role="list">
-        <Link href="/">
-          <a>Home</a>
+      <div className="">
+        <Link href="/" role="listitem">
+          <a className >Home</a>
         </Link>
         <Link href="/">
           <a>about</a>
--- pages/_document.js
+++ pages/_document.js
@@ -1,25 +1,30 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
   }
 
   render() {
     return (
       <Html lang="en">
         <Head>
-                <link href="//fonts.gstatic.com" rel="preconnect" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,700;1,400&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
