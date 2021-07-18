347b4ae3de2b14aedd8a55e26bd6b0809a506d79
ashishmangla <ashishmangla1991@gmail.com>
2021-07-18 16:43:41 +0530

    add new images

--- components/tools/tools.jsx
+++ components/tools/tools.jsx
@@ -7,9 +7,25 @@ const tools = [
   { name: "swagger" },
   { name: "Jira" },
   { name: "Bitbucket" },
+  { name: "Trello" },
+  { name: "BrowserStack" },
+  { name: "Jira" },
+  { name: "HTML5" },
+  { name: "CSS3" },
+  { name: "swagger" },
+  { name: "Jira" },
+  { name: "Bitbucket" },
   { name: "trello" },
   { name: "BrowserStack" },
   { name: "Jira" },
+  { name: "HTML5" },
+  { name: "CSS3" },
+  { name: "swagger" },
+  { name: "Jira" },
+  { name: "Bitbucket" },
+  { name: "trello" },
+  { name: "BrowserStack" },
+  { name: "Jira" }
 ];
 
 const Tools = () => {
@@ -17,18 +33,17 @@ const Tools = () => {
     <Section className={style.tools}>
       <img src='shape-1.png' role="presentation" aria-hidden="true" className={style.tools__shape1} />
       <div className={style.tools__pattren1}></div>
-      <div className={style.tools__pattren2}></div>
       <div className="container">
         <div className={`row ${style.tools__list}`}>
           {tools.map((tool) => (
-            <div className={`col ${style.tools__item}`}>
+            <div className={`col ${style.tools__item}`} key={tool.name}>
               <div className={style.tools__wrap}>
                 <Image
                   placeholder="blur"
                   src={`https://res.cloudinary.com/dcqiq1pu5/image/upload/v1626277693/tools/${tool.name}.png`}
                   alt={`${tool.name}`}
-                  width={100}
-                  height={100}
+                  width={120}
+                  height={120}
                 />
               </div>
             </div>
